7e36aa2aa4cc77e8a2aa4f10009b67a4cbf8073a
fabrizia2 <melvinrenish@gmail.com>
2022-07-06 12:21:57 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,13 +4,16 @@
  * main - creates an infinite loop.
  * Return: 0
  */
+
 int main(void)
 {
 
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+	
 	i = 0;
+	
 	/**
 	 * while (i < 10)
 	 * {
@@ -18,6 +21,5 @@ int main(void)
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
