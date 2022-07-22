d097fea4a1f713832bcc8c9dbedb63d7526577aa
fabrizia2 <melvinrenish@gmail.com>
2022-07-22 05:54:41 -0700

    changes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,22 +5,21 @@
  * main - multiplies two numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: result
+ * Return: multiple
  */
 
 int main(int argc, char **argv)
 {
 	int multiple = atoi(argv[1]) * atoi(argv[2]);
 
-	if (argc == 3)
-	{
-		printf("%d\n", multiple);
-	}
-	else
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else
+		printf("%d\n", multiple);
+
 
 	return (0);
 }
