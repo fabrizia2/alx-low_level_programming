73e3acb01cfd0e31ef62ebfe098548ce9036634f
fabrizia2 <melvinrenish@gmail.com>
2022-07-28 12:50:50 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -54,19 +54,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 		return (str);
 }
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *concat;
-
-	concat = string_nconcat("Best ", "School !!!", 6);
-	printf("%s\n", concat);
-	free(concat);
-	return (0);
-}
