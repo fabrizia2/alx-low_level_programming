a6743f4f7bf49f407bcb4705e0edaf04252e5a32
fabrizia2 <melvinrenish@gmail.com>
2022-07-28 16:11:51 -0700

    a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,22 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _realloc - reallocates a mem block using mallock && free
- * @ptr: pointer to a mem previously allocated
- * @old_size: the size of allocated space for ptr
- * @new_size: new size of new mem
- *
- * Return: ptr
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: void pointer
+ * @old_size: already allocated size
+ * @new_size: new size to allocate
+ * Return: pointer to newly allocated memory or null
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
 
-	char *str;
-	unsigned int k = 0;
+{
+	unsigned char *str;
+	unsigned int k;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -27,22 +24,30 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		ptr = malloc(sizeof(void *) * new_size);
+		ptr = malloc(new_size * sizeof(void *));
 		if (ptr == NULL)
 			return (NULL);
 		return (ptr);
 	}
-	str = malloc(sizeof(char) * new_size);
+	str = malloc(new_size * sizeof(char));
 	if (str == NULL)
 		return (NULL);
+	k = 0;
 	if (new_size > old_size)
 	{
-		while (k < old_size || k < new_size)
+		while (k < old_size)
 		{
 			str[k] = ((char *)ptr)[k];
+			k++;
 		}
-	}
-
 		free(ptr);
 		return (str);
+	}
+	while (k < new_size)
+	{
+		str[k] = ((char *)ptr)[k];
+		k++;
+	}
+	free(ptr);
+	return (str);
 }
