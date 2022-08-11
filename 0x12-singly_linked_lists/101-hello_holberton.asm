7446222dc785c7993a3b6742dbd5678930d6c004
fabrizia2 <melvinrenish56@gmail.com>
2022-08-11 11:34:43 -0700

    a 64-bit program in assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+;done by Melvin Renish
+;Superb
+;Assembly in c
+
+	global main
+	extern printf
+
+	section .text
+main:
+	mov  rdi, format
+	mov  rax, 0
+	call  printf
+	mov  rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
