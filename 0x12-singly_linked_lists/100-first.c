cffb68240b96b9482f4542838f1e4e4b7ed5fc47
fabrizia2 <melvinrenish56@gmail.com>
2022-08-11 11:19:47 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myFuncode - runs before main
+ */
+
+void myFuncode(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
