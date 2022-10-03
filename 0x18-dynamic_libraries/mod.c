4dce3653dc310735f2147b514886278214cc2a83
fabrizia2 <melvinrenish56@gmail.com>
2022-10-03 13:38:23 -0700

    a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mod - divides two int and returns remainder
+ *
+ * @a: first integer
+ * @b: second integer
+ * 
+ * Return: sum of the two integers
+ */
+int mod(int a, int b)
+{
+	int mod;
+	mod = a % b;
+	return (mod);
+}
